32858e82a61fce187a422e3997df634385f05f15
edgarp <eperez@centralmedia.mx>
2025-10-17 23:25:13 -0500

    adding_new_hasing

--- app/utils/hashing.py
+++ app/utils/hashing.py
@@ -3,6 +3,7 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_password_hash(password: str):
+    password = password[:72]  # 🔹 bcrypt solo admite 72 bytes
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
